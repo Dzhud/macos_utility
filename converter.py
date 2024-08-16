19ba2c935a8b670d6100153b6b1b856268d2c43e
Adeleke Omotayo <omotayo8@live.com>
2024-08-16 18:12:40 +0100

    Docx2Pdf integration

--- converter.py
+++ converter.py
@@ -7,7 +7,9 @@ doc.save("Output.pdf")
 #Next thing is to remove Aspose Watermark programmtically. 
 '''
 from docx2pdf import convert
-
+import os
+# One File Converter
+'''
 def convert_word_to_pdf(word_file, pdf_file):
     try:
         convert(word_file, pdf_file)
@@ -19,4 +21,26 @@ def convert_word_to_pdf(word_file, pdf_file):
 word_file_path = "test_file.docx"
 pdf_file_path = "docx2pdf.pdf"
 convert_word_to_pdf(word_file_path, pdf_file_path)
+'''
+# Multiple File Converter
+def bulk_convert_to_pdf(folder_path):
+    try:
+        convert(folder_path)
+        print(f"Converted DOCX files in {folder_path} to PDF successfully.")
+    except Exception as e:
+        print(f"Error: {e}")
 
+# Example usage
+folder_path = "/path/to/your/docx/folder"
+bulk_convert_to_pdf(folder_path)
+'''
+def bulk_convert_to_pdf():
+    try:
+        current_directory = os.getcwd()
+        convert(current_directory)
+        print(f"\n\nConverted DOCX files in {current_directory} to PDF successfully.\n\n")
+    except Exception as e:
+        print(f"Error: {e}")
+
+bulk_convert_to_pdf()
+'''
